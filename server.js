ae9ce666fb3b6970f0109398233eccc6461c50e4
amanduke <65379991+amanduke@users.noreply.github.com>
2020-11-22 18:11:00 -0500

    heroku

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const PORT = process.env.PORT || 3001;
 const sequelize = require("./config/connection");
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+//test
+
 const sess = {
   secret: 'secret',
   cookie: {},
